ee95165a57ce5a181c39e1d501dddd87fd188165
Warley Júnior <warley@coontrol.com.br>
2022-08-19 14:05:53 -0300

    aula 05

--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -1,7 +1,8 @@
+import { ITarefa } from '../../../types/tarefas'
 import style from '../list.module.scss'
 
 
-export default function Item({tarefa, tempo} : {tarefa: string, tempo: string}){
+export default function Item({tarefa, tempo, selecionado, completado, id} : ITarefa){
     return(
     <li className={style.item}>
     <h3>{tarefa}</h3>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Forms';
 import Lista from '../components/List';
+import { ITarefa } from '../types/tarefas';
 import style from './app.module.scss';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
       <Cronometro />
     </div>
   );
