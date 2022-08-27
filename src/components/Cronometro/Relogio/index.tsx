2691ad6c8c18642fb45918b16a9768aab445950d
Warley Júnior <warley@coontrol.com.br>
2022-08-27 18:39:06 -0300

    finalizando o curso

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,14 +1,23 @@
-import React from "react";
-import style from './relogio.module.scss';
+import style from './Relogio.module.scss';
 
-export default function Relogio() {
-    return(
-        <React.Fragment>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioDivisao}>:</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
-        </React.Fragment>
-    )  
+interface Props {
+  tempo: number | undefined
+}
+
+export default function Relogio({ tempo = 0 }: Props) {
+  const minutos = Math.floor( tempo / 60);
+  const minutoDezena = Math.floor( minutos / 10);
+  const minutoUnidade = minutos % 10;
+  const segundos = tempo % 60;
+  const segundoDezena = Math.floor( segundos / 10);
+  const segundoUnidade = segundos % 10;
+  return (
+    <>
+      <span className={style.relogioNumero}>{minutoDezena}</span>
+      <span className={style.relogioNumero}>{minutoUnidade}</span>
+      <span className={style.relogioDivisao}>:</span>
+      <span className={style.relogioNumero}>{segundoDezena}</span>
+      <span className={style.relogioNumero}>{segundoUnidade}</span>
+    </>
+  )
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,49 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Cronometro from '../components/Cronometro';
-import Formulario from '../components/Forms';
-import Lista from '../components/List';
-import { ITarefa } from '../types/tarefas';
-import style from './app.module.scss';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
+import style from './App.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
+
   return (
     <div className={style.AppStyle}>
-      <Formulario setTarefas={setTarefas}/>
-      <Lista tarefas={tarefas}/>
-      <Cronometro />
+      <Formulario setTarefas={setTarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
